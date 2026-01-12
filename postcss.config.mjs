51bb0289ff68f1d1ebb1b961687271f46fa40b9a
Talal Irfan <talalirfan4@gmail.com>
2026-01-13 03:03:25 +0500

    Fix Tailwind + React + Next build issues

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+export default {
   plugins: {
-    "@tailwindcss/postcss": {}, // Update this line
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
-
-export default config;
